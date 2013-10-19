4a512840a97f38c569051678fe409db972241b30
Benedikt Vogler <e-mail@benediktvogler.de>
2013-10-19 22:13:33 +0200

    You can now hide the diagram

--- com/BombingGames/EngineCore/FPSdiag.java
+++ com/BombingGames/EngineCore/FPSdiag.java
@@ -11,8 +11,9 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 public class FPSdiag {
     private final int[] data = new int[10];
     private float timeSinceUpdate;
-    private int field;
+    private int field;//the current field number
     private final int xPos, yPos, width;
+    private boolean visible;
 
     /**
      *
@@ -46,28 +47,30 @@ public class FPSdiag {
      * @param view
      */
     public void render(View view){
-        ShapeRenderer shRenderer = view.getShapeRenderer();
-        shRenderer.begin(ShapeRenderer.ShapeType.FilledRectangle);
-        for (int i = 0; i < data.length; i++) { //render each field in memory
-            if (i == field) //highlight current FPS
-                shRenderer.setColor(Color.PINK);
-            else shRenderer.setColor(Color.WHITE);
-            shRenderer.filledRect(xPos+width*i, yPos-data[i], width-1, data[i]);
-        }
-        shRenderer.end();
-        
-        //render average
-        int avg = getAverage();
-        shRenderer.begin(ShapeRenderer.ShapeType.Line);
-        shRenderer.setColor(Color.BLUE);
-        shRenderer.line(xPos, yPos-avg, xPos+width*data.length, yPos-avg);
-        
-        shRenderer.setColor(Color.GRAY);
-        shRenderer.line(xPos, yPos, xPos+width*data.length, yPos);
-        shRenderer.line(xPos, yPos-30, xPos+width*data.length, yPos-30);
-        shRenderer.line(xPos, yPos-60, xPos+width*data.length, yPos-60);
-        shRenderer.line(xPos, yPos-120, xPos+width*data.length, yPos-120);
-        shRenderer.end();
+        if (visible){
+            ShapeRenderer shRenderer = view.getShapeRenderer();
+            shRenderer.begin(ShapeRenderer.ShapeType.FilledRectangle);
+            for (int i = 0; i < data.length; i++) { //render each field in memory
+                if (i == field) //highlight current FPS
+                    shRenderer.setColor(Color.PINK);
+                else shRenderer.setColor(Color.WHITE);
+                shRenderer.filledRect(xPos+width*i, yPos-data[i], width-1, data[i]);
+            }
+            shRenderer.end();
+
+            //render average
+            int avg = getAverage();
+            shRenderer.begin(ShapeRenderer.ShapeType.Line);
+            shRenderer.setColor(Color.BLUE);
+            shRenderer.line(xPos, yPos-avg, xPos+width*data.length, yPos-avg);
+
+            shRenderer.setColor(Color.GRAY);
+            shRenderer.line(xPos, yPos, xPos+width*data.length, yPos);
+            shRenderer.line(xPos, yPos-30, xPos+width*data.length, yPos-30);
+            shRenderer.line(xPos, yPos-60, xPos+width*data.length, yPos-60);
+            shRenderer.line(xPos, yPos-120, xPos+width*data.length, yPos-120);
+            shRenderer.end();
+     }
     }
     
     /**
@@ -92,5 +95,21 @@ public class FPSdiag {
         }
         if (length > 0) avg /= length;
         return avg;
+    }
+
+    /**
+     * Is the diagramm visible?
+     * @return 
+     */
+    public boolean isVisible() {
+        return visible;
+    }
+
+   /**
+    * Set the FPSdiag visible. You must nevertheless call render().
+    * @param visible 
+    */
+    public void setVisible(boolean visible) {
+        this.visible = visible;
     }
 }

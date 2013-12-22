bf0e2514fd051e0adc69d7bafa93e84a782c1aa6
Benedikt Vogler <e-mail@benediktvogler.de>
2013-12-22 19:22:01 +0100

    Barrel loads sound from assetmanager

--- com/BombingGames/WurfelEngine/Core/Gameobjects/ExplosiveBarrel.java
+++ com/BombingGames/WurfelEngine/Core/Gameobjects/ExplosiveBarrel.java
@@ -3,7 +3,7 @@ package com.BombingGames.WurfelEngine.Core.Gameobjects;
 import com.BombingGames.WurfelEngine.Core.Controller;
 import com.BombingGames.WurfelEngine.Core.Map.AbstractPosition;
 import com.BombingGames.WurfelEngine.Core.Map.Coordinate;
-import com.badlogic.gdx.Gdx;
+import com.BombingGames.WurfelEngine.WEMain;
 import com.badlogic.gdx.audio.Sound;
 
 /**
@@ -26,7 +26,8 @@ public class ExplosiveBarrel extends Block implements IsSelfAware {
         if (coords == null) throw new NullPointerException("No coordinates given to ExplosiveBarrel during creation."); 
         this.coords = coords;
         setObstacle(true);
-        if (explosionsound == null) explosionsound = Gdx.audio.newSound(Gdx.files.internal("com/BombingGames/WurfelEngine/Game/Sounds/explosion2.ogg"));
+        if (explosionsound == null)
+            explosionsound = WEMain.getAsset("com/BombingGames/WurfelEngine/Game/Sounds/explosion2.ogg");
     }
     
     /**

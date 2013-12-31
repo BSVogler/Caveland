1e48ac713bacc274872f81121180930a5bb555e3
Benedikt Vogler <e-mail@benediktvogler.de>
2013-12-31 15:13:55 +0100

    v1.2 player class

--- com/BombingGames/WurfelEngine/Core/Gameobjects/Player.java
+++ com/BombingGames/WurfelEngine/Core/Gameobjects/Player.java
@@ -1,6 +1,35 @@
+/*
+ * Copyright 2013 Benedikt Vogler.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, 
+ *   this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice, 
+ *   this list of conditions and the following disclaimer in the documentation 
+ *   and/or other materials provided with the distribution.
+ * * Neither the name of Bombing Games nor Benedikt Vogler nor the names of its contributors 
+ *   may be used to endorse or promote products derived from this software without specific
+ *   prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 package com.BombingGames.WurfelEngine.Core.Gameobjects;
 
 import com.BombingGames.WurfelEngine.Core.Map.Point;
+import com.BombingGames.WurfelEngine.WEMain;
 import com.badlogic.gdx.Gdx;
 
 
@@ -17,10 +46,21 @@ public class Player extends AbstractCharacter{
      */
     public Player(int id, Point point) {
         super(id, 3, point);
-        setFallingSound(Gdx.audio.newSound(Gdx.files.internal("com/BombingGames/Game/Sounds/wind.ogg")));
-        setRunningSound(Gdx.audio.newSound(Gdx.files.internal("com/BombingGames/Game/Sounds/victorcenusa_running.ogg")));
-        setJumpingSound(Gdx.audio.newSound(Gdx.files.internal("com/BombingGames/Game/Sounds/jump_man.wav")));
-        setLandingSound(Gdx.audio.newSound(Gdx.files.internal("com/BombingGames/Game/Sounds/landing.wav")));
+        Gdx.app.debug("Player", "Creating player");
+        setFallingSound(
+            (com.badlogic.gdx.backends.openal.Ogg.Sound)
+            WEMain.getAsset("com/BombingGames/WurfelEngine/Game/Sounds/wind.ogg")
+        );
+        setRunningSound(
+            (com.badlogic.gdx.backends.openal.Ogg.Sound)
+            WEMain.getAsset("com/BombingGames/WurfelEngine/Game/Sounds/victorcenusa_running.ogg")
+        );
+        setJumpingSound((com.badlogic.gdx.backends.openal.Wav.Sound)
+            WEMain.getAsset("com/BombingGames/WurfelEngine/Game/Sounds/jump_man.wav")
+        );
+        setLandingSound((com.badlogic.gdx.backends.openal.Wav.Sound)
+            WEMain.getAsset("com/BombingGames/WurfelEngine/Game/Sounds/landing.wav")
+        );
         
         setTransparent(true);
         setObstacle(true);
@@ -34,9 +74,19 @@ public class Player extends AbstractCharacter{
     public void jump() {
         super.jump(5);
     }
-
+    
     @Override
-    public float[] getAiming() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    /**
+     * Getting aim relative to player by reading mouse position.
+     */
+    public float[] getAiming(){
+       float deltaX =Gdx.input.getX() - this.getPos().get2DPosX();
+       float deltaY =Gdx.input.getY() - this.getPos().get2DPosY(); 
+       float length = (float) Math.sqrt( Math.pow(deltaX,2)+ Math.pow(deltaY,2));
+       return new float[]{
+            deltaX/length,
+            deltaY*2/length,
+            0
+        };
     }
 }

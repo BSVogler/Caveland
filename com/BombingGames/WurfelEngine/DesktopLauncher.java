cca4d161f3d19686f656fc575d3ac63f275b0f0f
Benedikt Vogler <e-mail@benediktvogler.de>
2014-03-28 22:19:45 +0100

    added first things for map editor

--- com/BombingGames/WurfelEngine/DesktopLauncher.java
+++ com/BombingGames/WurfelEngine/DesktopLauncher.java
@@ -4,6 +4,8 @@ import com.BombingGames.WurfelEngine.Core.BasicMainMenu.BasicMainMenu;
 import com.BombingGames.WurfelEngine.Core.BasicMainMenu.BasicMenuItem;
 import com.BombingGames.WurfelEngine.Core.Controller;
 import com.BombingGames.WurfelEngine.Core.View;
+import com.BombingGames.WurfelEngine.MapEditor.MapEditorController;
+import com.BombingGames.WurfelEngine.MapEditor.MapEditorView;
 
 /**
  *A test class for starting the engine.
@@ -19,8 +21,9 @@ public class DesktopLauncher {
         
         BasicMenuItem[] menuItems = new BasicMenuItem[]{
             new BasicMenuItem(0, "Load Map", Controller.class, View.class, new Configuration()),
-            new BasicMenuItem(1, "Options"),
-            new BasicMenuItem(2, "Exit")
+            new BasicMenuItem(1, "Map Editor", MapEditorController.class, MapEditorView.class, new Configuration()),
+            new BasicMenuItem(2, "Options"),
+            new BasicMenuItem(3, "Exit")
         };   
         
         WE.setMainMenu(new BasicMainMenu(menuItems));

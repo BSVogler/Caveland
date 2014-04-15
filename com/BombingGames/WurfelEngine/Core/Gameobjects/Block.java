ed3788d7673f409c82b02e7e522bda3e308f8c4b
Benedikt Vogler <e-mail@benediktvogler.de>
2014-04-15 23:22:46 +0200

    throw error if no sprite sheet found

--- com/BombingGames/WurfelEngine/Core/Gameobjects/Block.java
+++ com/BombingGames/WurfelEngine/Core/Gameobjects/Block.java
@@ -235,7 +235,8 @@ public class Block extends AbstractGameObject {
      * @return an sprite of the side
      */
     public static AtlasRegion getBlockSprite(int id, int value, int side) {
-        if (getSpritesheet() == null) return null;
+        if (getSpritesheet() == null) throw new NullPointerException("No spritesheet found.");
+        
         if (blocksprites[id][value][side] == null){ //load if not already loaded
             AtlasRegion sprite = getSpritesheet().findRegion(CATEGORY+Integer.toString(id)+"-"+value+"-"+side);
             if (sprite == null){ //if there is no sprite show the default "sprite not found sprite" for this category

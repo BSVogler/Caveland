0a9dc3a7c4873bbb94b4088121d0db385061dee1
Cbeed <e-mail@benediktvogler.de>
2014-09-01 19:26:13 +0200

    javadoc

--- source/com/BombingGames/WurfelEngine/Core/Map/Coordinate.java
+++ source/com/BombingGames/WurfelEngine/Core/Map/Coordinate.java
@@ -365,6 +365,11 @@ public class Coordinate extends AbstractPosition {
 
     /**
      * Get the neighbour coordinates of the neighbour of the coords you give.
+     * 7 \ 0 / 1<br>
+     * -------<br>
+     * 6 | 8 | 2<br>
+     * -------<br>
+     * 5 / 4 \ 3<br>
      * @param neighbourSide the side number of the given coordinates
      * @return The coordinates of the neighbour.
      */

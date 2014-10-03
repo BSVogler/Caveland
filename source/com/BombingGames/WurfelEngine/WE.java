cb659177f921dd56e289c5bad595267c02c345fe
Cbeed <e-mail@benediktvogler.de>
2014-10-03 17:01:06 +0200

    version string

--- source/com/BombingGames/WurfelEngine/WE.java
+++ source/com/BombingGames/WurfelEngine/WE.java
@@ -53,13 +53,13 @@ import java.io.File;
  *The Main class of the engine. To create a new engine use  {@link com.BombingGames.WurfelEngine.WE#construct(java.lang.String, java.lang.String[]) }
  * The Wurfel Engine needs Java >1.7 and the API libGDX0.9.9
  * @author Benedikt S. Vogler
- * @version 1.3.3
+ * @version 1.3.4
  */
 public class WE extends Game {
     /**
      * The version of the Engine
      */
-    public static final String VERSION = "1.3.3";    
+    public static final String VERSION = "1.3.4";    
     private static File workingDirectory;
     private static boolean fullscreen = false;
     private static WE instance;

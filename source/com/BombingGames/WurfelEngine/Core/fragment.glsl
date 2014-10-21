b35f09bde01a8b4a2584a795e24d3b24cf4b51be
Cbeed <e-mail@benediktvogler.de>
2014-10-21 19:06:27 +0200

    removed redundant uniform

--- source/com/BombingGames/WurfelEngine/Core/fragment.glsl
+++ source/com/BombingGames/WurfelEngine/Core/fragment.glsl
@@ -5,7 +5,6 @@
 varying vec4 v_color;
 varying vec2 v_texCoords;
 uniform sampler2D u_texture;
-uniform mat4 u_projTrans;
 
 void main() {
     gl_FragColor = v_color * texture2D(u_texture, v_texCoords);

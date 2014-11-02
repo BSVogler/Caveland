a5a710f0edfeb26eeb2b70425087c278d09a22d8
Cbeed <e-mail@benediktvogler.de>
2014-11-02 10:41:02 +0100

    increased map version string
    to 0.13

--- source/com/BombingGames/WurfelEngine/Core/Map/MapMetaData.java
+++ source/com/BombingGames/WurfelEngine/Core/Map/MapMetaData.java
@@ -50,7 +50,7 @@ public class MapMetaData {
     /**
      *
      */
-    public static final String VERSION = "0.12";
+    public static final String VERSION = "0.13";
     private int chunkBlocksX;
     private int chunkBlocksY;
     private int chunkBlocksZ;

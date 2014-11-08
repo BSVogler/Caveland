dfd691e83dcdc1dc5969e822ca7f9964bcbb8db1
BSVogler <e-mail@benediktvogler.de>
2014-11-08 21:50:20 +0100

    Added credits screen
    #166

---
+++ source/com/BombingGames/Caveland/MainMenu/CreditsScreen.java
@@ -0,0 +1,118 @@
+/*
+ * If this software is used for a game the official „Wurfel Engine“ logo or its name must be visible in an intro screen or main menu.
+ *
+ * Copyright 2014 Benedikt Vogler.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, 
+ *   this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice, 
+ *   this list of conditions and the following disclaimer in the documentation 
+ *   and/or other materials provided with the distribution.
+ * * Neither the name of Benedikt Vogler nor the names of its contributors 
+ *   may be used to endorse or promote products derived from this software without specific
+ *   prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.BombingGames.Caveland.MainMenu;
+
+import com.BombingGames.Caveland.Caveland;
+import com.BombingGames.WurfelEngine.WE;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.utils.Align;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+
+/**
+ *
+ * @author Benedikt Vogler
+ */
+public class CreditsScreen implements Screen {
+    private final Stage stage;
+    private final OrthographicCamera camera;
+    //private final TestActor actor;
+
+    /**
+     *
+     */
+    public CreditsScreen() {
+        stage = new Stage(new FitViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight()), WE.getEngineView().getBatch());
+        WE.getEngineView().addInputProcessor(stage);
+                
+		Label credits = new Label(Caveland.getCredits(), WE.getEngineView().getSkin());
+		credits.setWidth(500);
+		credits.setAlignment(Align.center);
+		credits.setPosition(stage.getWidth()/2-credits.getWidth()/2, 150);
+		stage.addActor(credits);
+        
+        //set the center to the top left
+        camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        camera.setToOrtho(true, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        
+    }
+
+    @Override
+    public void render(float delta) {
+        //clear & set background to black
+        Gdx.gl20.glClearColor(0.75f, 0.63f, 0.36f, 1f );
+        Gdx.gl20.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        
+        //update camera and set the projection matrix
+        camera.update();
+        WE.getEngineView().getBatch().setProjectionMatrix(camera.combined);
+        WE.getEngineView().getShapeRenderer().setProjectionMatrix(camera.combined);
+        
+        stage.draw();
+        WE.getEngineView().getBatch().begin();
+		WE.getEngineView().getFont().draw(WE.getEngineView().getBatch(), "FPS:"+ Gdx.graphics.getFramesPerSecond(), 20, 20);
+		WE.getEngineView().getFont().draw(WE.getEngineView().getBatch(), Gdx.input.getX()+ ","+Gdx.input.getY(), Gdx.input.getX(), Gdx.input.getY());
+        WE.getEngineView().getBatch().end();
+        WE.updateAndRender(delta*1000f);
+    }
+
+    @Override
+    public void resize(int width, int height) {
+    }
+
+    @Override
+    public void show() {
+    }
+
+    @Override
+    public void hide() {
+    }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void resume() {
+    }
+
+    @Override
+    public void dispose() {
+        stage.dispose();
+    }
+
+    
+}
+

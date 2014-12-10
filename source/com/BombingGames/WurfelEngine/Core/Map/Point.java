75b835d6d344c9919c9e2baaa11dcbb4615832cf
BSVogler <e-mail@benediktvogler.de>
2014-12-10 19:05:18 +0100

    added equals and hashCode for point

--- source/com/BombingGames/WurfelEngine/Core/Map/Point.java
+++ source/com/BombingGames/WurfelEngine/Core/Map/Point.java
@@ -573,4 +573,25 @@ public class Point extends AbstractPosition {
 
         return result;
 	}
+	
+	@Override
+	public boolean equals(Object obj){
+		if (this == obj) return true;
+		if (obj == null) return false;
+		if (getClass() != obj.getClass()) return false;
+		Point other = (Point)obj;
+		if (x != other.x) return false;
+		if (y != other.y) return false;
+		return z == other.z;
+	}
+
+	@Override
+	public int hashCode() {
+		int hash = 7;
+		hash = 97 * hash + Float.floatToIntBits(this.x);
+		hash = 97 * hash + Float.floatToIntBits(this.y);
+		hash = 97 * hash + Float.floatToIntBits(this.z);
+		return hash;
+	}
+
 }

47bfb1de634968a99c0c71661684a2cc96d420a0
BSVogler <e-mail@benediktvogler.de>
2015-01-17 15:45:32 +0100

    registering cvar playerfriction

--- source/com/BombingGames/WurfelEngine/Core/CVar.java
+++ source/com/BombingGames/WurfelEngine/Core/CVar.java
@@ -112,6 +112,7 @@ public class CVar {
 		register("playerWalkingSpeed", 4.0f, CVarFlags.CVAR_ARCHIVE);
 		register("timeSpeed",1f, CVarFlags.CVAR_VOLATILE);
 		register("friction",0.2f, CVarFlags.CVAR_ARCHIVE);
+		register("playerfriction",0.2f, CVarFlags.CVAR_ARCHIVE);
 	}
 	
 	/**

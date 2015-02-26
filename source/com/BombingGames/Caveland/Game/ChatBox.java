4e07ce05e32e54ad7988643f021fbfafc92a28ca
BSVogler <e-mail@benediktvogler.de>
2015-02-26 22:33:51 +0100

    improved chatbox

--- source/com/BombingGames/Caveland/Game/ChatBox.java
+++ source/com/BombingGames/Caveland/Game/ChatBox.java
@@ -1,6 +1,7 @@
 package com.BombingGames.Caveland.Game;
 
 import com.BombingGames.WurfelEngine.WE;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Window;
 
@@ -11,9 +12,11 @@ import com.badlogic.gdx.scenes.scene2d.ui.Window;
 public class ChatBox extends Window {
 	private String text;
 
-	public ChatBox(String text) {
-		super("Person who speaks", WE.getEngineView().getSkin());
-		
+	public ChatBox(Stage stage, String title, String text) {
+		super(title, WE.getEngineView().getSkin());
+		setPosition(stage.getWidth()/2, stage.getHeight());
+		setWidth(600);
+		setHeight(200);
 		addActor(new Label(text, WE.getEngineView().getSkin()));
 	}
 	

aad9aa861dfab941304337d20959645b8d51d436
BSVogler <e-mail@benediktvogler.de>
2015-03-02 16:50:22 +0100

    don't shade fragments which have no normal map

--- source/com/BombingGames/WurfelEngine/Core/fragmentNM.fs
+++ source/com/BombingGames/WurfelEngine/Core/fragmentNM.fs
@@ -16,22 +16,28 @@ uniform vec4 moonColor;
 void main() {
     //RGBA of our diffuse color
     vec4 DiffuseColor = texture2D(u_texture, v_texCoords);
-	vec3 sunLight = vec3(0,0,0);
-	vec3 moonLight = vec3(0,0,0);
+	vec3 normalColor = texture2D(u_normals, v_texCoords).rgb;
+
+	//don't shade fragment's where there is no normal map
+	if (DiffuseColor.a >0.01 && DiffuseColor.r==normalColor.r && DiffuseColor.g==normalColor.g && DiffuseColor.b==normalColor.b) {
+		gl_FragColor = DiffuseColor;
+	} else{
+		vec3 sunLight = vec3(0,0,0);
+		vec3 moonLight = vec3(0,0,0);
 	
-	//RGB of our normal map
-	vec3 N = (texture2D(u_normals, v_texCoords).rgb*2.0- 1.0);//-0.058)*1.25 to normalize because normals are not 100% correct
-	N.x = -N.x;//x is flipped in texture, so fix this in shaders
 
-	//check if sun is shining, if not ignore it
-	if (sunColor.r > 0.05 || sunColor.g > 0.05 || sunColor.b> 0.05) {
-		sunLight = vec3(sunColor) * max(dot(N, sunNormal), 0.0);
+		vec3 N = (normalColor*2.0- 1.0);//-0.058)*1.25 to normalize because normals are not 100% correct
+		N.x = -N.x;//x is flipped in texture, so fix this in shaders
+
+		//check if sun is shining, if not ignore it
+		if (sunColor.r > 0.05 || sunColor.g > 0.05 || sunColor.b> 0.05) {
+			sunLight = vec3(sunColor) * max(dot(N, sunNormal), 0.0);
+		}
+
+		//check if moon is shining, if not ignore it
+		if (moonColor.r > 0.05 || moonColor.g > 0.05 || moonColor.b> 0.05) {
+			moonLight = vec3(moonColor) * max(dot(N, moonNormal), 0.0);
+		}
+		gl_FragColor = vec4(v_color.rgb+vec3(0.5,0.5,0.5),v_color.a) * vec4((sunLight+moonLight+ambientColor.rgb)*DiffuseColor.rgb,DiffuseColor.a);
 	}
-
-	//check if moon is shining, if not ignore it
-	if (moonColor.r > 0.05 || moonColor.g > 0.05 || moonColor.b> 0.05) {
-		moonLight = vec3(moonColor) * max(dot(N, moonNormal), 0.0);
-	}
-
-	gl_FragColor = vec4(v_color.rgb+vec3(0.5,0.5,0.5),v_color.a) * vec4((sunLight+moonLight+ambientColor.rgb)*DiffuseColor.rgb,DiffuseColor.a);
 }

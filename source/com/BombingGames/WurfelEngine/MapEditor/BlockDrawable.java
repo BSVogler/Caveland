7c70e8b65450c155b174fa4a6ca1e2c434b6b936
BSVogler <e-mail@benediktvogler.de>
2015-03-06 22:52:16 +0100

    passing null as color

--- source/com/BombingGames/WurfelEngine/MapEditor/BlockDrawable.java
+++ source/com/BombingGames/WurfelEngine/MapEditor/BlockDrawable.java
@@ -33,7 +33,6 @@ package com.BombingGames.WurfelEngine.MapEditor;
 
 import com.BombingGames.WurfelEngine.Core.Gameobjects.Block;
 import com.BombingGames.WurfelEngine.WE;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 
@@ -74,7 +73,7 @@ public class BlockDrawable extends TextureRegionDrawable {
     @Override
     public void draw(Batch batch, float x, float y, float width, float height) {
 		if (block.getId()!=0) {
-			block.render(WE.getEngineView(), (int) (x+this.x), (int) y, Color.GRAY.cpy(), true);
+			block.render(WE.getEngineView(), (int) (x+this.x), (int) y, null, true);
 		}
     }
 

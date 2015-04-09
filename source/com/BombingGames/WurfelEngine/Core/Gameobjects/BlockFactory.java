2e02bba2ceccb07380b0387462da9f1e7612dbbf
BSVogler <e-mail@benediktvogler.de>
2015-04-09 11:13:18 +0200

    javadoc

--- source/com/BombingGames/WurfelEngine/Core/Gameobjects/BlockFactory.java
+++ source/com/BombingGames/WurfelEngine/Core/Gameobjects/BlockFactory.java
@@ -12,12 +12,16 @@ public interface BlockFactory {
 	
     /**
      * Check if the block is liquid.
+	 * @param id
+	 * @param value
      * @return true if liquid, false if not 
      */
 	public boolean isLiquid(int id, int value);
 	
     /**
      * Is the block a true block with sides or represents it another thing like a flower?
+	 * @param id
+	 * @param value
      * @return 
      */
 	public boolean hasSides(int id, int value);

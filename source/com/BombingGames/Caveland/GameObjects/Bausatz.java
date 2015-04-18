e1870d03f35b80e89f2d0884f8f4319777130bae
BSVogler <e-mail@benediktvogler.de>
2015-04-18 17:29:13 +0200

    added class for Bausatz

---
+++ source/com/BombingGames/Caveland/GameObjects/Bausatz.java
@@ -0,0 +1,16 @@
+package com.BombingGames.Caveland.GameObjects;
+
+import com.BombingGames.WurfelEngine.Core.Gameobjects.SimpleEntity;
+
+/**
+ *
+ * @author Benedikt Vogler
+ */
+public class Bausatz extends SimpleEntity {
+	private static final long serialVersionUID = 1L;
+
+	public Bausatz() {
+		super((byte) 52);
+	}
+	
+}

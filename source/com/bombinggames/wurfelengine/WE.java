4b11362886fa28771cd296e5057cdf024ff6c656
BSVogler <e-mail@benediktvogler.de>
2015-07-28 17:26:47 +0200

    changed version string

--- source/com/bombinggames/wurfelengine/WE.java
+++ source/com/bombinggames/wurfelengine/WE.java
@@ -69,7 +69,7 @@ public class WE {
     /**
      * The version of the Engine
      */
-    public static final String VERSION = "1.6.2";  
+    public static final String VERSION = "1.6.3";  
 	private static final File workingDirectory = WorkingDirectory.getWorkingDirectory();
 
 	/**

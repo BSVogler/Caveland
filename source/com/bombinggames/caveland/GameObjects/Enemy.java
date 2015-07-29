5021a9f496b06b5bd75fe552fee5175122549206
BSVogler <e-mail@benediktvogler.de>
2015-07-29 23:14:13 +0200

    added new particle

--- source/com/bombinggames/caveland/GameObjects/Enemy.java
+++ source/com/bombinggames/caveland/GameObjects/Enemy.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.math.Vector3;
 import com.bombinggames.wurfelengine.core.Controller;
 import com.bombinggames.wurfelengine.core.Gameobjects.AbstractEntity;
 import com.bombinggames.wurfelengine.core.Gameobjects.Block;
+import com.bombinggames.wurfelengine.core.Gameobjects.DestructionParticle;
 import com.bombinggames.wurfelengine.core.Gameobjects.EntityAnimation;
 import com.bombinggames.wurfelengine.core.Gameobjects.MovableEntity;
 import com.bombinggames.wurfelengine.core.Gameobjects.SimpleEntity;
@@ -25,6 +26,11 @@ public class Enemy extends MovableEntity{
     private Point lastPos;
 	private long movementSoundPlaying;
 	private float mana = 1000;
+	private int attackInProgess = 0;
+	/**
+	 * in m/s
+	 */
+	private float movementSpeed = 2;
     
 	/**
 	 *
@@ -63,36 +69,45 @@ public class Enemy extends MovableEntity{
 		//update as usual
         super.update(dt);
 		
+		if (attackInProgess > 0) {
+			attackInProgess -= dt;
+			movementSpeed = 0;
+		}
+
+		//clamp at 0
+		if (attackInProgess < 0) {
+			attackInProgess = 0;
+			movementSpeed = 2;
+			playMovementAnimation();
+		}
+			
         if (isSpawned() && getPosition().toCoord().isInMemoryAreaHorizontal()) {
             //follow the target
             if (target != null && target.isSpawned()) {
-				Vector3 d = new Vector3();
+				if (getPosition().distanceTo(target) > Block.GAME_EDGELENGTH*1.5f) {
+					//movement logic
+					Vector3 d = new Vector3();
 
-				d.x = target.getPosition().getX()-getPosition().getX();
-                d.y = target.getPosition().getY()-getPosition().getY();
-				d.nor();//direction only
-				d.scl(2f);
-				d.z = getMovement().z;
+					d.x = target.getPosition().getX()-getPosition().getX();
+					d.y = target.getPosition().getY()-getPosition().getY();
+					d.nor();//direction only
+					d.scl(movementSpeed);//speed at 2 m/s
+					d.z = getMovement().z;
 
-				// update the movement vector
-				setMovement(d);
+					setMovement(d);// update the movement vector
 
-				if (getPosition().distanceTo(target)<120) {
-					//setSpeed(0);
-					mana = ((int) (mana+dt));
-                    if (mana>=1000){
-                        mana = 0;//reset
-                        new SimpleEntity((byte) 46).spawn(getPosition().cpy()).setAnimation(
-							new EntityAnimation(new int[]{300}, true, false)
-						);
-                        target.damage((byte)50);
-                    }
-				}// else 
-					//setSpeed(0.4f);
+					mana = ((int) (mana + dt));
+				}
+				
+				//attack
+				if (attackInProgess == 0) {
+					performAttack();
+				}
             }
 			
+			
 			//find nearby target if there is none
-			if (target==null){
+			if (target == null){
 				ArrayList<CustomPlayer> nearby = getPosition().getEntitiesNearbyHorizontal(Block.GAME_DIAGLENGTH*4, CustomPlayer.class);
 				if (!nearby.isEmpty())
 					target = nearby.get(0);
@@ -126,14 +141,18 @@ public class Enemy extends MovableEntity{
 	@Override
 	public void damage(byte value) {
 		super.damage(value);
-		if (getHealth()<=0){
+		if (getHealth() <= 0){
+			new DestructionParticle((byte) 34).spawn(getPosition().toPoint().cpy());
+			new DestructionParticle((byte) 35).spawn(getPosition().toPoint().cpy());
+			new DestructionParticle((byte) 36).spawn(getPosition().toPoint().cpy());
+			
 			Controller.getSoundEngine().stop(movementSound, movementSoundPlaying);
 			if (getHealth() <= 0 && killSound != null)
 				Controller.getSoundEngine().play(killSound);
 			killcounter++;
 		}
 	}
-
+	
 	/**
 	 *
 	 * @return
@@ -145,5 +164,17 @@ public class Enemy extends MovableEntity{
 	@Override
 	public MovableEntity clone() throws CloneNotSupportedException {
 		return super.clone();
+	}
+
+	private void performAttack() {
+		if (mana >= 1000 && getPosition().distanceTo(target) < Block.GAME_EDGELENGTH*2f ){
+			mana = 0;//reset
+			new SimpleEntity((byte) 33).spawn(getPosition().cpy()).setAnimation(
+				new EntityAnimation(new int[]{300}, true, false)
+			);
+			target.damage((byte)50);
+			pauseMovementAnimation();
+			attackInProgess = 1500;//1500ms until the attack is done
+		}
 	}
 }

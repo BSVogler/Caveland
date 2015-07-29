b150e3412c6c0997124a084141f7f13818a2cf1e
BSVogler <e-mail@benediktvogler.de>
2015-07-30 00:14:50 +0200

    added attack animation

--- source/com/bombinggames/caveland/GameObjects/Enemy.java
+++ source/com/bombinggames/caveland/GameObjects/Enemy.java
@@ -1,5 +1,6 @@
 package com.bombinggames.caveland.GameObjects;
 
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 import com.bombinggames.wurfelengine.core.Controller;
 import com.bombinggames.wurfelengine.core.Gameobjects.AbstractEntity;
@@ -17,16 +18,23 @@ import java.util.ArrayList;
  */
 public class Enemy extends MovableEntity{
 	private static final long serialVersionUID = 1L;
+	/**
+	 * the time for the attack animation
+	 */
+	public static final float ATTACKTIME = 600;
+	private static final String KILLSOUND = "robot1destroy";
+	private static final String MOVEMENTSOUND = "robot1Wobble";
 	private static int killcounter = 0;
-	private static final String killSound = "robot1destroy";
-	private static final String movementSound = "robot1Wobble";
 	
     private transient MovableEntity target;
     private int runningagainstwallCounter = 0;
     private Point lastPos;
 	private long movementSoundPlaying;
 	private float mana = 1000;
-	private int attackInProgess = 0;
+	/**
+	 * countdown while the attack ins in progress. Used for animation.
+	 */
+	private float attackInProgess = 0;
 	/**
 	 * in m/s
 	 */
@@ -54,7 +62,7 @@ public class Enemy extends MovableEntity{
 
 	@Override
 	public AbstractEntity spawn(final Point point) {
-		movementSoundPlaying = Controller.getSoundEngine().loop(movementSound, point);
+		movementSoundPlaying = Controller.getSoundEngine().loop(MOVEMENTSOUND, point);
 		return super.spawn(point);
 	}
 
@@ -72,6 +80,46 @@ public class Enemy extends MovableEntity{
 		if (attackInProgess > 0) {
 			attackInProgess -= dt;
 			movementSpeed = 0;
+			Vector2 orientation = getOrientation();
+			if (orientation.x < -Math.sin(Math.PI/3)){
+				setValue((byte) 1);//west
+			} else {
+				if (orientation.x < - 0.5){
+					//y
+					if (orientation.y<0){
+						setValue((byte) 2);//north-west
+					} else {
+						setValue((byte) 0);//south-east
+					}
+				} else {
+					if (orientation.x <  0.5){
+						//y
+						if (orientation.y<0){
+							setValue((byte) 3);//north
+						}else{
+							setValue((byte) 7);//south
+						}
+					}else {
+						if (orientation.x < Math.sin(Math.PI/3)) {
+							//y
+							if (orientation.y < 0){
+								setValue((byte) 4);//north-east
+							} else{
+								setValue((byte) 6);//sout-east
+							}
+						} else{
+							setValue((byte)5);//east
+						}
+					}
+				}
+			}
+			if (attackInProgess > ATTACKTIME*2/3f) {
+				setValue((byte) (getValue()+40));
+			} else if (attackInProgess > ATTACKTIME/3f){
+				setValue((byte) (getValue()+48));
+			} else {
+				setValue((byte) (getValue()+56));
+			}
 		}
 
 		//clamp at 0
@@ -146,9 +194,9 @@ public class Enemy extends MovableEntity{
 			new DestructionParticle((byte) 35).spawn(getPosition().toPoint().cpy());
 			new DestructionParticle((byte) 36).spawn(getPosition().toPoint().cpy());
 			
-			Controller.getSoundEngine().stop(movementSound, movementSoundPlaying);
-			if (getHealth() <= 0 && killSound != null)
-				Controller.getSoundEngine().play(killSound);
+			Controller.getSoundEngine().stop(MOVEMENTSOUND, movementSoundPlaying);
+			if (getHealth() <= 0 && KILLSOUND != null)
+				Controller.getSoundEngine().play(KILLSOUND);
 			killcounter++;
 		}
 	}
@@ -174,7 +222,7 @@ public class Enemy extends MovableEntity{
 			);
 			target.damage((byte)50);
 			pauseMovementAnimation();
-			attackInProgess = 1500;//1500ms until the attack is done
+			attackInProgess = ATTACKTIME;//1500ms until the attack is done
 		}
 	}
 }

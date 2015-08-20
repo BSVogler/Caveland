93e45fdbe8b158170ffd8b97bfd56ff522b1081e
BSVogler <e-mail@benediktvogler.de>
2015-08-20 18:33:29 +0200

    removed license

--- source/com/bombinggames/caveland/GameObjects/logicblocks/LiftLogic.java
+++ source/com/bombinggames/caveland/GameObjects/logicblocks/LiftLogic.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.bombinggames.caveland.GameObjects.logicblocks;
 
 import com.bombinggames.caveland.Game.CustomGameView;

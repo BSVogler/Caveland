7678abe6077bb87a395948b25110c215ef97c699
BSVogler <e-mail@benediktvogler.de>
2015-10-08 18:23:11 +0200

    format bullet, reduced crashes

--- source/com/bombinggames/wurfelengine/extension/shooting/Bullet.java
+++ source/com/bombinggames/wurfelengine/extension/shooting/Bullet.java
@@ -41,138 +41,147 @@ import com.bombinggames.wurfelengine.core.Gameobjects.SimpleEntity;
 import java.util.ArrayList;
 
 /**
- *A bullet is a moving object which can destroy and damage entities or the world.
+ * A bullet is a moving object which can destroy and damage entities or the
+ * world.
+ *
  * @author Benedikt Vogler
  */
 public class Bullet extends AbstractEntity {
+
 	private static final long serialVersionUID = 1L;
-    private static String explosionsound = "explosion2";
-    private Vector3 dir = new Vector3();//movement
-    private float speed;
-    private byte damage;
-    private int distance =0;//distance traveled
-    private MovableEntity parent;//no self shooting
-    private int maxDistance = 1000;//default maxDistance
-    private int explosive = 0;
-    private int impactSprite;
-    
-    /**
-	 * You can set a different sprite via {@link #setSpriteId(byte)}. It uses the engine default sprite.
-     * @see #setSpriteId(byte) 
-     */
-    public Bullet(){
-        super((byte) 12);//default graphics id is 12
-    }
+	private static String explosionsound = "explosion2";
+	private Vector3 dir = new Vector3();//movement
+	private float speed;
+	private byte damage;
+	private int distance = 0;//distance traveled
+	private MovableEntity parent;//no self shooting
+	private int maxDistance = 1000;//default maxDistance
+	private int explosive = 0;
+	private int impactSprite;
 
-    @Override
-    public void update(float dt) {
+	/**
+	 * You can set a different sprite via {@link #setSpriteId(byte)}. It uses
+	 * the engine default sprite.
+	 *
+	 * @see #setSpriteId(byte)
+	 */
+	public Bullet() {
+		super((byte) 12);//default graphics id is 12
+	}
+
+	@Override
+	public void update(float dt) {
 		super.update(dt);
-        //dir.z=-delta/(float)maxDistance;//fall down
-        Vector3 dMov = dir.cpy().scl(dt*speed);
-        //dMov.z /= 1.414213562f;//mixed screen and game space together?
-        getPosition().addVector(dMov);
-        setRotation(getRotation()+dt);
-        
-        //only spawn specific distance then destroy self
-        distance += dMov.len();
-        if (distance > maxDistance)
-            dispose();
-                
-        //block hit -> spawn effect
-        if (getPosition().isInMemoryAreaHorizontal() && getPosition().getBlock().isObstacle()){
-            if (impactSprite!= 0)
-                new SimpleEntity((byte) 31).spawn(getPosition().cpy()).setAnimation(new EntityAnimation(new int[]{1000} , true, false));
-            dispose();
-        }
-        
+		//dir.z=-delta/(float)maxDistance;//fall down
+		Vector3 dMov = dir.cpy().scl(dt * speed);
+		//dMov.z /= 1.414213562f;//mixed screen and game space together?
+		getPosition().addVector(dMov);
+		setRotation(getRotation() + dt);
+
+		//only spawn specific distance then destroy self
+		distance += dMov.len();
+		if (distance > maxDistance) {
+			dispose();
+		}
+
+		//block hit -> spawn effect
+		if (getPosition().getBlock() != null && getPosition().getBlock().isObstacle()) {
+			if (impactSprite != 0) {
+				new SimpleEntity((byte) 31).spawn(getPosition().cpy()).setAnimation(new EntityAnimation(new int[]{1000}, true, false));
+			}
+			dispose();
+		}
+
         //check character hit
-         //get every character on this coordinate
-        ArrayList<MovableEntity> entitylist;
-        entitylist = Controller.getMap().getEntitysOnCoord(getPosition().toCoord(), MovableEntity.class);
-        entitylist.remove(parent);//remove self from list to prevent self shooting
-        if (!entitylist.isEmpty()) {
-            entitylist.get(0).damage(damage);//damage only the first unit on the list
-            new SimpleEntity((byte) 16).spawn(getPosition().cpy());//spawn blood
-            dispose();
-        }
-    }
+		//get every character on this coordinate
+		ArrayList<MovableEntity> entitylist;
+		entitylist = Controller.getMap().getEntitysOnCoord(getPosition().toCoord(), MovableEntity.class);
+		entitylist.remove(parent);//remove self from list to prevent self shooting
+		if (!entitylist.isEmpty()) {
+			entitylist.get(0).damage(damage);//damage only the first unit on the list
+			new SimpleEntity((byte) 16).spawn(getPosition().cpy());//spawn blood
+			dispose();
+		}
+	}
 
-    /**
-     *
-     * @param dir
-     */
-    public void setDirection(Vector3 dir) {
-        this.dir = dir;
-    }
-    
-    /**
-     *
-     * @param speed
-     */
-    public void setSpeed(float speed){
-        this.speed = speed;
-    }
+	/**
+	 *
+	 * @param dir
+	 */
+	public void setDirection(Vector3 dir) {
+		this.dir = dir;
+	}
 
-    /**
-     *
-     * @param parent
-     */
-    public void setParent(MovableEntity parent) {
-        this.parent = parent;
-    }
-    
-    /**
-     *
-     * @param maxDistance
-     */
-    public void setMaxDistance(int maxDistance){
-        this.maxDistance = maxDistance;
-    }
+	/**
+	 *
+	 * @param speed
+	 */
+	public void setSpeed(float speed) {
+		this.speed = speed;
+	}
 
-    /**
-     *
-     * @param damage
-     */
-    public void setDamage(byte damage) {
-        this.damage = damage;
-    }
-    
-    /**
-     *
-     * @param ex
-     */
-    public void setExplosive(int ex){
-        explosive = ex;
-    }
-    
-      /**
-     * Spawns explosion.
-     */
-    private void explode(int radius){
-       new Explosion(radius,(byte)80,WE.getGameplay().getView().getCameras().get(0)).spawn(getPosition());
-    }
+	/**
+	 *
+	 * @param parent
+	 */
+	public void setParent(MovableEntity parent) {
+		this.parent = parent;
+	}
 
-    @Override
-    public void dispose() {
-        if (explosive>0) explode(3);
-        super.dispose();
-    }
+	/**
+	 *
+	 * @param maxDistance
+	 */
+	public void setMaxDistance(int maxDistance) {
+		this.maxDistance = maxDistance;
+	}
 
-    /**
-     * Set the sprite which get spawned when the bullet hits.
-     * @param id  if you don't want an impact sprite set id to0.
-     */
-    public void setImpactSprite(int id) {
-        impactSprite = id;
-    }
+	/**
+	 *
+	 * @param damage
+	 */
+	public void setDamage(byte damage) {
+		this.damage = damage;
+	}
 
-    /**
-     *
-     * @return the distance traveled.
-     */
-    public int getDistance() {
-        return distance;
-    }
-    
-    
+	/**
+	 *
+	 * @param ex
+	 */
+	public void setExplosive(int ex) {
+		explosive = ex;
+	}
+
+	/**
+	 * Spawns explosion.
+	 */
+	private void explode(int radius) {
+		new Explosion(radius, (byte) 80, WE.getGameplay().getView().getCameras().get(0)).spawn(getPosition());
+	}
+
+	@Override
+	public void dispose() {
+		if (explosive > 0) {
+			explode(3);
+		}
+		super.dispose();
+	}
+
+	/**
+	 * Set the sprite which get spawned when the bullet hits.
+	 *
+	 * @param id if you don't want an impact sprite set id to0.
+	 */
+	public void setImpactSprite(int id) {
+		impactSprite = id;
+	}
+
+	/**
+	 *
+	 * @return the distance traveled.
+	 */
+	public int getDistance() {
+		return distance;
+	}
+
 }

35c5e37078a00374ed16a404ed20749fe7cddac3
BSVogler <e-mail@benediktvogler.de>
2015-10-16 12:23:45 +0200

    format

--- source/com/bombinggames/wurfelengine/core/Gameobjects/Explosion.java
+++ source/com/bombinggames/wurfelengine/core/Gameobjects/Explosion.java
@@ -13,9 +13,10 @@ import java.util.ArrayList;
  * @author Benedikt Vogler
  */
 public class Explosion extends AbstractEntity {
+
 	private static final long serialVersionUID = 1L;
 	private static String explosionsound;
-	
+
 	private final int radius;
 	private final byte damage;
 	private transient Camera camera;
@@ -24,28 +25,28 @@ public class Explosion extends AbstractEntity {
 	 * simple explosion without screen shake. Default radius is 2. Damage 500.
 	 */
 	public Explosion() {
-		super((byte)0);
+		super((byte) 0);
 		this.radius = 2;
 		damage = 50;
 		setSaveToDisk(false);
 	}
 
-	
 	/**
-	 * 
+	 *
 	 * @param radius the radius in game world blocks
 	 * @param damage Damage at center.
 	 * @param camera can be null. used for screen shake
 	 */
 	public Explosion(int radius, byte damage, Camera camera) {
-		super((byte)0);
+		super((byte) 0);
 		this.radius = radius;
 		this.damage = damage;
-		if (explosionsound == null)
-            explosionsound = "explosion";
+		if (explosionsound == null) {
+			explosionsound = "explosion";
+		}
 		this.camera = camera;
 		setSaveToDisk(false);
-    }
+	}
 
 	@Override
 	public void update(float dt) {
@@ -53,48 +54,50 @@ public class Explosion extends AbstractEntity {
 
 	/**
 	 * explodes
-	 * @return 
+	 *
+	 * @return
 	 */
-	
 	@Override
 	public AbstractEntity spawn(Point point) {
 		super.spawn(point);
 		//replace blocks by air
-		for (int x=-radius; x<radius; x++){
-			for (int y=-radius*2; y<radius*2; y++) {
-				for (int z=-radius; z<radius; z++){
+		for (int x = -radius; x < radius; x++) {
+			for (int y = -radius * 2; y < radius * 2; y++) {
+				for (int z = -radius; z < radius; z++) {
 					Coordinate coord = point.cpy().toCoord().addVector(x, y, z);
-					if (x*x + (y/2)*(y/2)+ z*z <= radius*radius){//check if in radius
-						coord.damage((byte) (damage*(1-(x*x + (y/2)*(y/2)+ z*z)/(radius*radius))));
-						
+					if (x * x + (y / 2) * (y / 2) + z * z <= radius * radius) {//check if in radius
+						coord.damage((byte) (damage * (1 - (x * x + (y / 2) * (y / 2) + z * z) / (radius * radius))));
+
 						//get every entity which is attacked
-						ArrayList<MovableEntity> list =
-							Controller.getMap().getEntitysOnCoord(
+						ArrayList<MovableEntity> list
+							= Controller.getMap().getEntitysOnCoord(
 								coord,
 								MovableEntity.class
 							);
 						for (MovableEntity ent : list) {
 							ent.damage(damage);
 						}
-						
+
 						Particle dust = (Particle) new Particle(
 							(byte) 22,
 							1700
 						).spawn(coord.toPoint());//spawn at center
-						dust.setColor(new Color(0.5f,0.45f,0.4f,1f));
+						dust.setColor(new Color(0.5f, 0.45f, 0.4f, 1f));
 						dust.setType(ParticleType.FIRE);
 						dust.addMovement(
 							coord.toPoint().getVector().sub(point.getVector()).nor().scl(4f)
 						);//move from center to outside
 					}
 				}
-			}	
+			}
 		}
-		
-		if (camera!=null)
-			camera.shake(radius*100/3f, 100);
-		if (explosionsound != null)
+
+		if (camera != null) {
+			camera.shake(radius * 100 / 3f, 100);
+		}
+		if (explosionsound != null) {
 			WE.SOUND.play(explosionsound);
+		}
 		dispose();
 		return this;
 	}

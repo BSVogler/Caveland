8eb0b78ef3852b235150e7e94bab886f296f5b47
BSVogler <e-mail@benediktvogler.de>
2015-11-23 16:32:48 +0100

    fixed goToNeighbor

--- source/com/bombinggames/wurfelengine/core/Map/Coordinate.java
+++ source/com/bombinggames/wurfelengine/core/Map/Coordinate.java
@@ -389,28 +389,28 @@ public class Coordinate extends AbstractPosition {
 				y -= 2;
 				break;
 			case 1:
-				x += getY() % 2 == 0 ? 1 : 0;
+				x += getY() % 2 == 0 ? 0 : 1;
 				y--;
 				break;
 			case 2:
 				x++;
 				break;
 			case 3:
-				x += getY() % 2 == 0 ? 1 : 0;
+				x += getY() % 2 == 0 ? 0 : 1;
 				y++;
 				break;
 			case 4:
 				y += 2;
 				break;
 			case 5:
-				x += getY() % 2 == 0 ? -1 : 0;
+				x -= getY() % 2 == 0 ? 1 : 0;
 				y++;
 				break;
 			case 6:
 				x--;
 				break;
 			case 7:
-				x += getY() % 2 == 0 ? -1 : 0;
+				x -= getY() % 2 == 0 ? 1 : 0;
 				y--;
 				break;
 		}

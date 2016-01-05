74b6156982287c929c1a5a89102cd4491c8eb88a
BSVogler <e-mail@benediktvogler.de>
2016-01-05 15:25:12 +0100

    robot drops resources at flag
    #356

--- source/com/bombinggames/caveland/GameObjects/SpiderRobot.java
+++ source/com/bombinggames/caveland/GameObjects/SpiderRobot.java
@@ -33,15 +33,17 @@ package com.bombinggames.caveland.GameObjects;
 import com.badlogic.gdx.ai.msg.MessageManager;
 import com.badlogic.gdx.math.Vector3;
 import com.bombinggames.caveland.Game.CavelandBlocks;
+import com.bombinggames.caveland.GameObjects.collectibles.Collectible;
 import com.bombinggames.wurfelengine.WE;
 import com.bombinggames.wurfelengine.core.Events;
 import com.bombinggames.wurfelengine.core.Gameobjects.Block;
 import com.bombinggames.wurfelengine.core.Map.Coordinate;
+import com.bombinggames.wurfelengine.core.Map.Point;
 import com.bombinggames.wurfelengine.extension.shooting.Laserdot;
 import java.util.ArrayList;
 
 /**
- *
+ * A robot can carry one collectible.
  * @author Benedikt Vogler
  */
 public class SpiderRobot extends Robot{
@@ -55,6 +57,8 @@ public class SpiderRobot extends Robot{
 	private transient float scanHeight;
 	private transient float laserRotate;
 	private transient Coordinate workingBlock;
+	private transient Coordinate storage;
+	private transient Collectible carry;
 
 	/**
 	 *
@@ -73,73 +77,80 @@ public class SpiderRobot extends Robot{
 		}
 		
 		if (hasPosition()) {
-			//look for resources
-			if (moveUp) {
-				scanHeight += dt / 300f;
-			} else {
-				scanHeight -= dt / 300f;
-			}
-			
-			if (scanHeight >= 1) {
-				moveUp = false;
-				scanHeight = 1;
-			}
+			if (laserdot != null) {
+				//look for resources
+				if (moveUp) {
+					scanHeight += dt / 300f;
+				} else {
+					scanHeight -= dt / 300f;
+				}
 
-			if (scanHeight <= -0.3f) {
-				moveUp = true;
-				scanHeight = -0.3f;
-			}
-			
-			if (moveRight) {
-				laserRotate += dt / 500f;
-			} else {
-				laserRotate -= dt / 500f;
-			}
-			
-			if (laserRotate >= 1) {
-				moveRight = false;
-				laserRotate = 1;
-			}
+				if (scanHeight >= 1) {
+					moveUp = false;
+					scanHeight = 1;
+				}
 
-			if (laserRotate <= -1) {
-				moveRight = true;
-				laserRotate = -1;
-			}
+				if (scanHeight <= -0.3f) {
+					moveUp = true;
+					scanHeight = -0.3f;
+				}
+
+				if (moveRight) {
+					laserRotate += dt / 500f;
+				} else {
+					laserRotate -= dt / 500f;
+				}
+
+				if (laserRotate >= 1) {
+					moveRight = false;
+					laserRotate = 1;
+				}
+
+				if (laserRotate <= -1) {
+					moveRight = true;
+					laserRotate = -1;
+				}
 			
-			if (laserdot == null){
-				laserdot = (Laserdot) new Laserdot().spawn(getPosition().cpy());
-				laserdot.setColor(COLORTEAM.cpy());
-			}
-			laserdot.update(
-				getAiming(),
-				getPosition().cpy().add(getOrientation().scl(20f)).add(0, 0, 20)
-			);
-			
-			Block block = laserdot.getPosition().getBlock();
-			if (block != null) {
-				byte id = block.getId();
-				if ((id == CavelandBlocks.CLBlocks.COAL.getId()
-					|| id == CavelandBlocks.CLBlocks.IRONORE.getId()
-					|| id == CavelandBlocks.CLBlocks.CRYSTAL.getId()
-					|| id == CavelandBlocks.CLBlocks.SULFUR.getId())
-				) {
-					if (workingBlock == null || workingBlock.getBlock() == null) {
-						workingBlock = new Coordinate(0, 0, 0);
-						workingBlock.setFromPoint(laserdot.getPosition());
-						//go to resources
-						MessageManager.getInstance().dispatchMessage(
-							this,
-							this,
-							Events.moveTo.getId(),
-							laserdot.getPosition().cpy()
-						);
+				laserdot.update(
+					getAiming(),
+					getPosition().cpy().add(getOrientation().scl(20f)).add(0, 0, 20)
+				);
+				
+				//find storage
+				if (storage == null) {
+					ArrayList<DropSpaceFlag> flagsnearby = laserdot.getPosition().getEntitiesNearby(Block.GAME_EDGELENGTH, DropSpaceFlag.class);
+					if (!flagsnearby.isEmpty()) {
+						storage = flagsnearby.get(0).getPosition().toCoord();
+					}
+				}
+
+				//find block to work on
+				Block block = laserdot.getPosition().getBlock();
+				if (block != null) {
+					byte id = block.getId();
+					if (id == CavelandBlocks.CLBlocks.COAL.getId()
+						|| id == CavelandBlocks.CLBlocks.IRONORE.getId()
+						|| id == CavelandBlocks.CLBlocks.CRYSTAL.getId()
+						|| id == CavelandBlocks.CLBlocks.SULFUR.getId()
+					) {
+						if (workingBlock == null || workingBlock.getBlock() == null) {
+							workingBlock = new Coordinate(0, 0, 0);
+							workingBlock.setFromPoint(laserdot.getPosition());
+							//go to resources
+							MessageManager.getInstance().dispatchMessage(
+								this,
+								this,
+								Events.moveTo.getId(),
+								laserdot.getPosition().cpy()
+							);
+						}
 					}
 				}
 			}
 			
-			//validate workinBlock
+			//validate workingBlock
 			if (workingBlock != null) {
-				block = workingBlock.getBlock();
+				Block block = workingBlock.getBlock();
 				if (block == null) {
 					workingBlock = null;
 				} else {
@@ -152,46 +163,86 @@ public class SpiderRobot extends Robot{
 						workingBlock = null;
 					}
 				}
+			} else {
+				enableScanner();
 			}
 			
-			
-			//gather resources
-			if (workingBlock != null) {
-				//if nearby
-				if (getPosition().distanceTo(workingBlock) < Block.GAME_EDGELENGTH*1.8f) {
-					MessageManager.getInstance().dispatchMessage(
+			if (carry != null) {
+				//move carry to storage
+				carry.setPosition(getPosition().cpy());
+				if (storage != null) {
+					if (getMovementAI() == null) {
+						MessageManager.getInstance().dispatchMessage(
+							this,
+							this,
+							Events.moveTo.getId(),
+							storage.toPoint()
+						);
+					}
+
+					//drop carry
+					if (getPosition().toCoord().equals(storage)) {
+						//verify flags existence
+						ArrayList<DropSpaceFlag> flagsnearby = storage.getEntitiesInside(DropSpaceFlag.class);
+						if (flagsnearby.isEmpty()) {
+							storage = null;
+						}
+						//place them on storage
+						if (storage != null) {
+							carry.setHidden(false);
+							carry.allowPickup();
+							carry.setPosition(storage);
+							carry = null;
+							MessageManager.getInstance().dispatchMessage(
+								this,
+								this,
+								Events.standStill.getId()
+							);
+						}
+					}
+				} else {
+					enableScanner();
+				}
+			} else {
+				//gather resources
+				if (workingBlock != null) {
+					//if nearby
+					if (getPosition().distanceTo(workingBlock) < Block.GAME_EDGELENGTH*1.8f) {
+						MessageManager.getInstance().dispatchMessage(
 							this,
 							this,
 							Events.standStill.getId()
 						);
-					if (performAttack()) {
-						workingBlock.damage((byte) 1);
-						if (workingBlock.getBlock() != null && workingBlock.getBlock().getHealth() % 8 == 0) {
-							CavelandBlocks.getLoot(workingBlock.getBlock().getId()).createInstance().spawn(getPosition().cpy());
+						if (performAttack()) {
+							workingBlock.damage((byte) 1);
+							WE.SOUND.play("impact");
+							if (workingBlock.getBlock() != null && workingBlock.getBlock().getHealth() % 8 == 0) {
+								carry = CavelandBlocks.getLoot(workingBlock.getBlock().getId()).createInstance();
+								carry.spawn(getPosition().cpy());
+								//carry.setHidden(true);
+								carry.preventPickup();
+							}
 						}
-					}
-				} else {
-					//if block is below move away
-					if (workingBlock.getZ()<getPosition().toCoord().getZ()) {
-						MessageManager.getInstance().dispatchMessage(
-							this,
-							this,
-							Events.moveTo.getId(),
-							workingBlock.cpy().add(1,0,0).toPoint()
-						);
 					} else {
+						Point moveToBlock = workingBlock.toPoint();
+						//if block is below move away
+						if (workingBlock.getZ() < getPosition().toCoord().getZ()) {
+							moveToBlock = workingBlock.cpy().add(1, 0, 0).toPoint();
+						}
 						MessageManager.getInstance().dispatchMessage(
 							this,
 							this,
 							Events.moveTo.getId(),
-							workingBlock.toPoint()
+							moveToBlock
 						);
 					}
 				}
 			}
-
-			//place them on storage
 			
+			if (storage != null && workingBlock != null) {
+				disableScanner();
+			}
+
 			//sound
 			if (getMovementHor().len2() > 0 && isOnGround()) {
 				if (walkingSound == 0l) {
@@ -240,5 +291,22 @@ public class SpiderRobot extends Robot{
 		walkingSound = 0;
 		laserdot.dispose();
 		super.disposeFromMap();
+	}
+	
+	/**
+	 * does nothijng if already on
+	 */
+	public void enableScanner() {
+		if (laserdot == null) {
+			laserdot = (Laserdot) new Laserdot().spawn(getPosition().cpy());
+			laserdot.setColor(COLORTEAM.cpy());
+		}
+	}
+
+	public void disableScanner() {
+		if (laserdot != null) {
+			laserdot.dispose();
+			laserdot = null;
+		}
 	}
 }

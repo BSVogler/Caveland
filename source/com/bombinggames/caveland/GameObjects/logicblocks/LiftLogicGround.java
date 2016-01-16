30d45acf5baf4c05b9e5f7bc170da1fd12794173
BSVogler <e-mail@benediktvogler.de>
2016-01-16 13:26:52 +0100

    can create basket

--- source/com/bombinggames/caveland/GameObjects/logicblocks/LiftLogicGround.java
+++ source/com/bombinggames/caveland/GameObjects/logicblocks/LiftLogicGround.java
@@ -3,8 +3,10 @@ package com.bombinggames.caveland.GameObjects.logicblocks;
 import com.badlogic.gdx.ai.msg.Telegram;
 import com.badlogic.gdx.ai.msg.Telegraph;
 import com.bombinggames.caveland.Game.CLGameView;
+import com.bombinggames.caveland.GameObjects.ExitPortal;
 import com.bombinggames.caveland.GameObjects.Interactable;
 import com.bombinggames.caveland.GameObjects.LiftBasket;
+import com.bombinggames.wurfelengine.WE;
 import com.bombinggames.wurfelengine.core.Gameobjects.AbstractBlockLogicExtension;
 import com.bombinggames.wurfelengine.core.Gameobjects.AbstractEntity;
 import com.bombinggames.wurfelengine.core.Gameobjects.Block;
@@ -19,6 +21,7 @@ import java.util.ArrayList;
 public class LiftLogicGround extends AbstractBlockLogicExtension implements Interactable, Telegraph {
 
 	private transient LiftBasket basket;
+	private LiftLogic interactionCreatesBasket;
 
 	/**
 	 *
@@ -40,7 +43,18 @@ public class LiftLogicGround extends AbstractBlockLogicExtension implements Inte
 		if (!possibleBaskets.isEmpty()) {
 			basket = possibleBaskets.get(0);
 		}
-		
+
+		if (basket == null) {
+			ArrayList<ExitPortal> possibleExitPortals = getPosition().getEntitiesNearbyHorizontal(Block.GAME_EDGELENGTH * 2, ExitPortal.class);
+			if (!possibleExitPortals.isEmpty()) {
+				try {
+					interactionCreatesBasket = (LiftLogic) possibleExitPortals.get(0).getTarget().getLogic();
+				} catch (NullPointerException ex) {
+
+				}
+			}
+		}
+
 		//stop at ground
 		if (basket != null && basket.getMovementDir() < 0 && basket.isOnGround() && getPosition().distanceToHorizontal(basket) < Block.GAME_EDGELENGTH) {
 			basket.setMovementDir(0);
@@ -56,26 +70,40 @@ public class LiftLogicGround extends AbstractBlockLogicExtension implements Inte
 	@Override
 	public void interact(CLGameView view, AbstractEntity actor) {
 		if (basket != null) {
-			
-			//start
-			if (basket.getMovementDir() == 0 && getPosition().distanceToHorizontal(basket) < Block.GAME_EDGELENGTH) {
-				basket.setMovementDir(1);
+			toggleBasket(actor);
+			return;
+		} else if (interactionCreatesBasket != null) {
+			if (basket == null) {
+				basket = new LiftBasket();
+			}
+			if (!basket.hasPosition()) {
+				basket.spawn(interactionCreatesBasket.getPosition().toPoint());
+			}
+			toggleBasket(actor);
+			return;
+		}
+		WE.SOUND.play("interactionFail", getPosition());
+	}
 
-				if (actor instanceof MovableEntity) {
-					basket.setPassenger((MovableEntity) actor);
-				}
-			} else {
-				//get back down
-				if (basket.getMovementDir() >= 0) {
-					basket.setMovementDir(-1);
-				}
+	private void toggleBasket(AbstractEntity actor) {
+		//start
+		if (basket.getMovementDir() == 0 && getPosition().distanceToHorizontal(basket) < Block.GAME_EDGELENGTH) {
+			basket.setMovementDir(1);
+
+			if (actor instanceof MovableEntity) {
+				basket.setPassenger((MovableEntity) actor);
+			}
+		} else //get back down
+		{
+			if (basket.getMovementDir() >= 0) {
+				basket.setMovementDir(-1);
 			}
 		}
 	}
 
 	@Override
 	public boolean interactable() {
-		return basket != null;
+		return true;
 	}
 
 	@Override

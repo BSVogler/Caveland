e5907f3d390b925584621be507690e35ad362753
BSVogler <e-mail@benediktvogler.de>
2016-01-26 12:13:39 +0100

    return object instance for air

--- source/com/bombinggames/wurfelengine/core/Gameobjects/Block.java
+++ source/com/bombinggames/wurfelengine/core/Gameobjects/Block.java
@@ -271,11 +271,7 @@ public class Block implements Serializable {
 	 * @return
 	 */
 	public RenderBlock toRenderBlock() {
-		if (id == 0) {
-			return null;
-		}
-		
-		if (id == 4) {//invisible wall
+		if (id == 0 || id == 4) {//invisible wall
 			RenderBlock a = new RenderBlock(this);
 			a.setHidden(true);
 			return a;

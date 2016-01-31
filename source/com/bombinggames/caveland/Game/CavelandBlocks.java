28e94bbc00d17a01c873027f2c2e6d95fb38a671
BSVogler <e-mail@benediktvogler.de>
2016-01-31 21:00:30 +0100

    fixed hiding bug of ido

--- source/com/bombinggames/caveland/Game/CavelandBlocks.java
+++ source/com/bombinggames/caveland/Game/CavelandBlocks.java
@@ -334,7 +334,7 @@ public class CavelandBlocks implements CustomBlocks {
 		if (id==13) return true;
 		if (id==14) return true;
 		if (id==15) return true;
-		if (id==17) return true;
+		if (id==17 && value==0) return true;
 		if (id==11) return true;
 		if (id==52) return true;
 		if (id==53) return true;

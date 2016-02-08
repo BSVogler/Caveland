d36d35269ce00fbcf9a2f3cdc6ed32bb489ca5d0
BSVogler <e-mail@benediktvogler.de>
2016-02-08 12:20:43 +0100

    changed version string

--- source/com/bombinggames/wurfelengine/WE.java
+++ source/com/bombinggames/wurfelengine/WE.java
@@ -68,14 +68,14 @@ import java.util.ArrayList;
  * and the API libGDX v1.8.0 (may work with older versions).
  *
  * @author Benedikt S. Vogler
- * @version 1.6.10
+ * @version 1.7.1
  */
 public class WE {
 
 	/**
 	 * The version of the Engine
 	 */
-	public static final String VERSION = "1.6.10";
+	public static final String VERSION = "1.7.1";
 	/**
 	 * the working directory where the config and files are saved
 	 */
